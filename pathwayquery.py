fc0949034068dd3df228da4791031b0cdb6e8bdc
astrika <astrika.0914@gmail.com>
2017-05-31 13:56:33 -0500

    successful pathway search

--- pathwayquery.py
+++ pathwayquery.py
@@ -1,9 +1,5 @@
 import pythoncyc
 #this creates PGDB object associated with meta(MetaCyc)
 meta = pythoncyc.select_organism('meta')
-compound = raw_input("Enter name of compound:")
-def search(compound):
-    pathways = meta.pathways_of_compound(compound)
-    print pathways
-    return pathways
-search(compound)
+pathways = meta.pathways_of_compound('urea')
+print pathways
