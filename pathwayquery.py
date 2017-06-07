a96d0654096f45b287601c0369465763206dd06b
astrika <astrika.0914@gmail.com>
2017-06-07 15:05:08 -0500

    hard-coded metabolite IDs

--- pathwayquery.py
+++ pathwayquery.py
@@ -1,9 +1,9 @@
 import sys
 import pythoncyc
 #this creates PGDB object associated with meta(MetaCyc)
-
 meta = pythoncyc.select_organism('meta')
-pathways = meta.pathways_of_compound(sys.argv[1])
+#lists pathways of specified compound
+pathways = meta.pathways_of_compound('sucrose')
 print pathways
 
 # compounds = meta.compounds_of_pathway('PWY-6552')
